dff66f5eba0098e52b4686a39154b6f038f468c4
Bryan MacFarlane <bryanmacf@gmail.com>
2020-07-03 10:25:49 -0400

    clean

--- frontend/public/script/index.tsx
+++ frontend/public/script/index.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-//import { Hello } from "../../views/components/hello";
 import { Clock } from "../../views/components/clock"
-
 import {IndexView} from '../../views/index'
-
-// TODO: pass state down and hydrate
-// https://dev.to/marvelouswololo/how-to-server-side-render-react-hydrate-it-on-the-client-and-combine-client-and-server-routes-1a3p
-// https://www.typescriptlang.org/docs/handbook/react-&-webpack.html#add-a-typescript-configuration-file
-/* <Hello name={window.__INITIAL__DATA__.name} */
 
 export interface CustomWindow extends Window {
     __INITIAL_STATE__: string;
 }
 
 declare let window: CustomWindow;
-
-// alert(JSON.stringify(window.__INITIAL_STATE__))
 
 let idxState: any = window.__INITIAL_STATE__
 
@@ -31,7 +22,6 @@ function newView(message: string) {
 ReactDOM.hydrate(
     newView(initMsg), 
     document.documentElement);
-    //document.getElementById("root"));
 
 // every .5 seconds update message
 // props are immutable so we have to construct a new view and render it on the parent element
@@ -45,7 +35,6 @@ setInterval(() => {
     }
 
     let msg = initMsg + marks;
-    //alert(msg);
     ReactDOM.render(newView(msg), document.documentElement)
 }, 500);
 
